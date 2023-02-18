1133b82d9e9d67702f075ae979a4f7f0ff1d42a8
mircoForcella <forcella1996@gmail.com>
2023-02-18 17:36:35 +0100

    Initial commit

---
+++ src/Start.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Start {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        while (true) {
+            System.out.print("Give number: ");
+            int saveInt = input.nextInt();
+            switch (saveInt) {
+                case 1:
+                    System.out.println("one");
+                    break;
+                case 2:
+                    System.out.println("two");
+                    break;
+                case 3:
+                    System.out.println("three");
+                    break;
+                case 4:
+                    System.out.println("four");
+                    break;
+                case 5:
+                    System.out.println("five");
+                    break;
+                case 6:
+                    System.out.println("six");
+                    break;
+                case 7:
+                    System.out.println("seven");
+                    break;
+                case 8:
+                    System.out.println("eight");
+                    break;
+                case 9:
+                    System.out.println("nine");
+                    break;
+                case 10:
+                    System.out.println("ten");
+                    break;
+                default:
+                    System.out.println("Cannot give you the name");
+            }
+        }
+    }
+}
